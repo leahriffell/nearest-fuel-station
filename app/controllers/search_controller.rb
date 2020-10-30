a98961e4910c5bddd2a233ac5bbdd3e477c7f913
Leah Riffell <leah.riffell@gmail.com>
2020-10-30 10:57:09 -0600

    Add WIP

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,7 @@ class SearchController < ApplicationController
     turing_lat = '39.750812'
     turing_long = '-104.996594'
     turing_address = '1331 17th St LL100, Denver, CO 80202'
+    # pass above in the params when user selects start point  
 
     @station = StationFacade.closest_electric(turing_lat, turing_long)
 
